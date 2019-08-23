1a78d70354f1f715a14ce3478aec1bbffb3df943
Dominic Parducci <dominic.parducci@Dominics-MacBook-Pro.local>
2019-08-23 15:36:18 -0700

    style form End/Intro

--- src/components/FormEnd/FormEnd.jsx
+++ src/components/FormEnd/FormEnd.jsx
@@ -5,8 +5,8 @@ import ParkingIcon from "./parking-icon.png";
 class FormEnd extends Component {
   render() {
     return (
-      <div>
-        <h2>Thank You!</h2>
+      <div className="wrapper-end">
+        <h2 className="end-header">Thank You!</h2>
 
         <div className="form-end-top">
           <p>
@@ -19,6 +19,27 @@ class FormEnd extends Component {
           </p>
           <div className="img-park">
             <img src={ParkingIcon} alt="" />
+          </div>
+          <div className="middle-text">
+            <p>
+              Meanwhile, please refer to the maps of{" "}
+              <span id="green-text">green street parking zones</span> for
+              alternative overnight parking.
+            </p>
+            <p id="end-italic">
+              *Always read signs and look out for “Real-time” hours
+            </p>
+          </div>
+          <div className="btns">
+            <a
+              href="https://www.safeparkingla.org/green-streets"
+              className="btn btn-primary btn-lg"
+              target="_blank"
+            >
+              LA GREEN STREETS
+            </a>
+
+            <button className="btn btn-primary btn-lg">RESOURCES</button>
           </div>
         </div>
       </div>
