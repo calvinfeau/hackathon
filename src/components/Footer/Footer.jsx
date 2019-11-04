7fe3014c8434db5a926b4666bb4ed1e952cdf5e4
Calvin Féau <calvin@calvins-mbp.lan>
2019-11-03 23:27:36 -0800

    designed mobile / tablet views

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,4 @@
 import React, {Component} from "react";
-import {Link} from "react-router-dom";
 import styled, {css} from "styled-components";
 
 const Wrapper = styled.div`
@@ -10,6 +9,10 @@ padding: 5vh 0;
 display: flex;
 flex-direction: column;
 align-items: center;
+@media screen and (max-width: 1100px) {
+  height: 20vh;
+  padding: 0;
+}
 `;
 
 const Flex = styled.div`
@@ -20,15 +23,27 @@ justify-content: space-around;
 
 const Icons = styled(Flex)`
 height: 60%;
-width: 25%;
+width: 20%;
 align-items: center;
 padding: 0 0 5vh;
 >a {
-  font-size: 30px;
-  padding: 7px 15px;
+  font-size: 2vmax;
+  padding: 0.4vmax 1vmax;
   background-color: white;
   color: #1b1b1b;
   border-radius: 50%;
+}
+
+@media screen and (max-width: 1100px) {
+  width: 75%;
+  padding: 0;
+  >a {
+    font-size: 2.5vmax;
+    padding: 0.8vmax 1.6vmax;
+    background-color: white;
+    color: #1b1b1b;
+    border-radius: 50%;
+  }
 }
 `;
 
@@ -36,8 +51,9 @@ const Links = styled(Flex)`
 height: 40%;
 width: 50%;
 >a, >span {
+  width: 10%;
   color: white;
-  width: 5vw;
+  text-align: center;
   :hover {
     text-decoration: none;
   }
@@ -47,16 +63,15 @@ width: 50%;
     cursor: pointer;
   }
 }
+@media screen and (max-width: 1100px) {
+  width: 90%;
+  font-size: 1.2vmax;
+  >a, >span {
+    width: 20%;
+    text-align: center;
+  }
+}
 `;
-
-const Note = styled.div`
-display: none;
-color: white;
-${props => props.clicked && css`
-display: block;
-`}
-`;
-
 
 class Footer extends Component {
   constructor(props) {
@@ -72,15 +87,19 @@ class Footer extends Component {
     return (
       <Wrapper>
         <Icons>
-          <a href="https://www.facebook.com/SafeParkingLA/" target="_blank" rel="noopener noreferrer"><i class="fab fa-facebook-square footer-fabs" /></a>
-          <a href="https://www.instagram.com/safeparking_la/" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram footer-fabs" /></a>
-          <a href="mailto:info@safeparkingla.org"><i class="far fa-envelope footer-fabs" /></a>
+          <a href="https://www.facebook.com/SafeParkingLA/" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook-square footer-fabs" /></a>
+          <a href="https://www.instagram.com/safeparking_la/" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram footer-fabs" /></a>
+          <a href="mailto:info@safeparkingla.org"><i className="far fa-envelope footer-fabs" /></a>
         </Icons>
         <Links>
           <a href="https://www.safeparkingla.org/about" target="_blank" rel="noopener noreferrer">About</a>
           <a href="https://www.safeparkingla.org/faq" target="_blank" rel="noopener noreferrer">FAQ</a>
           <a href="https://safeparkingla.networkforgood.com/projects/68953-safe-parking-la" target="_blank" rel="noopener noreferrer">Donate</a>
-          <span clicked={this.state.clicked} onClick={this.handleClick}>Contribute<br /><Note clicked={this.state.clicked}>Coming&nbsp;soon!</Note></span>
+          <span clicked={`${this.state.clicked}`} onClick={this.handleClick}>Contribute<br />
+          {this.state.clicked ? 
+            <span clicked={`${this.state.clicked}`}>Coming&nbsp;soon!</span> : <span></span>
+          }
+          </span>
           <a href="https://www.safeparkingla.org/contact" target="_blank" rel="noopener noreferrer">Contact</a>
         </Links>
       </Wrapper>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,12 +5,17 @@ import image1 from "./image1.png";
 import image2 from "./image2.png";
 import image3 from "./image3.png";
 import image4 from "./image4.png";
+import mobile1 from "./mobile-image1.png";
 
 const Wrapper1 = styled.div`
 height: 90vh;
 width: 100vw;
-background-image: url(${props => props.image});
+background-image: url(${image1});
 background-size: cover;
+
+@media screen and (max-width: 1100px) {
+  background-image: none;
+}
 `;
 
 const Div1 = styled.div`
@@ -19,27 +24,58 @@ height: 100%;
 padding: 10%;
 display: flex;
 flex-direction: column;
-justify-content: space-between;
+
+@media screen and (max-width: 1100px) {
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  justify-content: space-around;
+  align-items: center;
+}
 `;
 
 const Title1 = styled.div`
 color: #12679b;
 font-weight: 700;
-font-size: 4em;
+font-size: 3.5vmax;
 line-height: 1.1;
 mix-blend-mode: normal;
 letter-spacing: 1.5px;
+padding-bottom: 15%;
+
+@media screen and (max-width: 1100px) {
+  text-align: center;
+  line-height: 1.2;
+}
+`;
+
+const ImgMobile = styled.div`
+@media screen and (max-width: 1100px) {
+  width: 100vw;
+  height: 30vh;
+  background-image: url(${mobile1});
+  background-size: cover;
+}
 `;
 
 const Text1 = styled.div`
 letter-spacing: 1.5px;
-font-size: 1.2em;
+font-size: 1.3vmax;
+line-height: 1.7;
+padding-bottom: 15%;
+
+@media screen and (max-width: 1100px) {
+  padding: 0 10%;
+  text-align: center;
+  font-size: 1.5vmax;
+  line-height: 1.7;
+}
 `;
 
 const Button1 = styled(Link)`
-font-size: 1em;
-width: 50%;
-padding: 15px;
+font-size: 1.2vmax;
+width: 65%;
+padding: 0.75vmax;
 color: white;
 background-color: #12679b;
 text-align: center;
@@ -50,9 +86,15 @@ letter-spacing: 1.5px;
 :hover{
   color: #12679b; 
   background-color: transparent; 
-  text-decoration: none;}
-`;
+  text-decoration: none;
+}
 
+@media screen and (max-width: 1100px) {
+  width: 85%;
+  padding: 5%;
+  font-size: 1.5vmax;
+}
+`;
 
 class Section1 extends Component {
   constructor(props) {
@@ -60,10 +102,11 @@ class Section1 extends Component {
   }
   render() {
     return (
-      <Wrapper1 image={this.props.image}>
+      <Wrapper1>
         <Div1>
           <Title1>Nobody Plans To&nbsp;Be&nbsp;Homeless</Title1>
-          <Text1>Safe Parking programs are a safe and legal homelessness intervention to stabilize and connect to resources for people who are living in their vehicles.<br />All program locations include access to  a restroom facility and the security of a guard.</Text1>
+          <ImgMobile />
+          <Text1>Safe Parking programs are a safe and legal homelessness intervention to stabilize and connect to resources for people who are living in&nbsp;their&nbsp;vehicles.<br />All program locations include access to  a restroom facility and the security of&nbsp;a&nbsp;guard.</Text1>
           <Button1 to="/forms/intro">FIND YOUR SPOT</Button1>
         </Div1>
       </Wrapper1>
@@ -78,7 +121,12 @@ width: 100vw;
 background-color: #D7E3EC;
 display: flex;
 flex-direction: column;
-font-size: 1.5em;
+font-size: 1.2vmax;
+
+@media screen and (max-width: 1100px) {
+  height: 25vh;
+  text-align: center;
+}
 `;
 
 const Title2 = styled.div`
@@ -87,7 +135,13 @@ height: 30%;
 display: flex;
 align-items: flex-end;
 justify-content: center;
-font-size: 1.5em;
+font-size: 1.5vmax;
+letter-spacing: 1.5px;
+
+
+@media screen and (max-width: 1100px) {
+  font-size: 1.7vmax;
+}
 `;
 
 const Items = styled.div`
@@ -100,7 +154,6 @@ height: 70%;
 const Item = styled(Items)`
 flex-direction: column;
 justify-content: space-between;
-font-size: 1em;
 `;
 
 class Section2 extends Component {
@@ -109,9 +162,9 @@ class Section2 extends Component {
       <Wrapper2>
           <Title2>The Process</Title2>
           <Items>
-              <Item>STEP 1 <i className="material-icons" style={{fontSize: "60px"}}>assignment</i> Fill out safe parking form</Item>
-              <Item>STEP 2 <i className="material-icons" style={{fontSize: "60px"}}>headset_mic</i> We will contact you in 24 hrs</Item>
-              <Item>STEP 3 <i className="material-icons" style={{fontSize: "60px"}}>local_parking</i> Receive a parking permit</Item>
+              <Item>STEP 1 <i className="material-icons" style={{fontSize: "3vmax"}}>assignment</i> Fill out safe parking&nbsp;form</Item>
+              <Item>STEP 2 <i className="material-icons" style={{fontSize: "3vmax"}}>headset_mic</i> We will contact you&nbsp;in&nbsp;24 hrs</Item>
+              <Item>STEP 3 <i className="material-icons" style={{fontSize: "3vmax"}}>local_parking</i> Receive a parking&nbsp;permit</Item>
           </Items>
       </Wrapper2>
     )
@@ -120,15 +173,21 @@ class Section2 extends Component {
 
 const Wrapper3 = styled.div`
 width: 100vw;
-height: 100vh;
-padding: 8vw;
+height: 90vh;
+padding: 5vw;
 display: flex;
 justify-content: space-between;
+
+@media screen and (max-width: 1100px) {
+  flex-direction: column;
+  padding: 0;
+  justify-content: start;
+  height: 100%;
+}
 `;
 
 const Card = styled.div`
 width: 30%;
-height: 110%
 display: flex;
 flex-direction: column;
 align-items: center;
@@ -139,30 +198,53 @@ align-items: center;
 ${props => props.card === 1 && css`background-color: #FFEEDA;`}
 ${props => props.card === 2 && css`background-color: #DBEDDD;`}
 ${props => props.card === 3 && css`background-color: #FFF7E2;`}
+
+@media screen and (max-width: 1100px) {
+  width: 100vw;
+  background-color: white;
+  height: auto;
+  margin-bottom: 10%;
+  >img {
+    height: auto;
+  }
+}
 `;
 
 const CardText = styled.div`
-padding: 7% 10% 7%;
-height: 60%;
+height: 55%;
+padding: 10%;
 display: flex;
 flex-direction: column;
 align-items: center;
-justify-content: space-between;
 line-height: 1.5;
-font-size: 1em;
+font-size: 1vmax;
 letter-spacing: 1.5px;
 >span {
   font-weight: 700;
-  font-size: 18px;
+  font-size: 1.5vmax;
+  padding-bottom: 10%;
+}
+
+@media screen and (max-width: 1100px) {
+  padding: 5%;
+  justify-content: start;
+  font-size: 1.5vmax;
+  text-align: center;
+  line-height: 2;
+  >span {
+    font-size: 2.5vmax;
+    padding-bottom: 5%;
+  }
 }
 `;
 
 const CardButton = styled.a`
-width: 60%;
-padding: 10px;
+width: 75%;
+padding: 0.5vmax;
+margin-top: auto;
 background-color: transparent;
 color: #12679b;
-font-size: 0.8em;
+font-size: 0.8vmax;
 text-align: center;
 font-weight: 700;
 border: 1px solid #12679b;
@@ -171,6 +253,23 @@ border-radius: 30px;
   color: white; 
   background-color: #12679b;
   text-decoration: none;
+}
+
+@media screen and (max-width: 1100px) {
+  font-size: 1.5vmax;
+  width: 85%;
+  margin-top: 10%;
+  padding: 5%;
+  ${props => props.card === 1 && css`--current-color: #E59638;`}
+  ${props => props.card === 2 && css`--current-color: #297F32;`}
+  ${props => props.card === 3 && css`--current-color: #E2B731;`}
+  color: var(--current-color);
+  border-color: var(--current-color);
+  :hover{
+    color: white; 
+    background-color: var(--current-color);
+    text-decoration: none;
+  }
 }
 `;
 
@@ -183,7 +282,7 @@ class Section3 extends Component {
             <CardText>
               <span>WHO WE SERVE</span>
               In Los Angeles County, there are over 15,700 people living in their vehicles each night. These vehicle dwellers represent over 25% of the population of people experiencing homelessness&nbsp;in&nbsp;LA&nbsp;County.
-            <CardButton href="https:www.safeparkingla.org/who-we-serve" target="_blank" rel="noopener noreferrer">READ MORE STORIES</CardButton>
+            <CardButton card={1} href="https:www.safeparkingla.org/who-we-serve" target="_blank" rel="noopener noreferrer">READ MORE STORIES</CardButton>
             </CardText>
           </Card>
 
@@ -192,7 +291,7 @@ class Section3 extends Component {
             <CardText>
             <span>CAR BUT NO HOME?</span>
             Safe Parking LA is an example of a relatively new effort on the West Coast to assist a specific group of individuals before they fall further to life on the street. Many of them may be working or have some source of income and are homeless for the&nbsp;first&nbsp;time. 
-            <CardButton href="https:www.safeparkingla.org/news" target="_blank" rel="noopener noreferrer">READ MORE NEWS</CardButton>
+            <CardButton card={2} href="https:www.safeparkingla.org/news" target="_blank" rel="noopener noreferrer">READ MORE NEWS</CardButton>
             </CardText>
           </Card>
 
@@ -201,7 +300,7 @@ class Section3 extends Component {
             <CardText>
             <span>FOR VETERANS</span>
             SafeParkingLA provides a safe and legal overnight parking place for veterans and their families who are currently living in their vehicles. We are in a secure lot with a portable bathroom&nbsp;and&nbsp;wash&nbsp;station.
-            <CardButton href="https:www.safeparkingla.org/resources-for-veterans" target="_blank" rel="noopener noreferrer">MORE INFO</CardButton>
+            <CardButton card={3} href="https:www.safeparkingla.org/resources-for-veterans" target="_blank" rel="noopener noreferrer">MORE INFO</CardButton>
             </CardText>
           </Card>
       </Wrapper3>
@@ -216,6 +315,7 @@ display: flex;
 flex-direction: column;
 align-items: center;
 margin: 5vh 0;
+>span {font-size: 1.3vmax;}
 `;
 
 const Title4 = styled.div`
@@ -223,24 +323,31 @@ font-weight: 700;
 display: flex;
 align-items: flex-end;
 justify-content: center;
-font-size: 1.5em;`;
+font-size: 1.5vmax;
+
+@media screen and (max-width: 1100px) {
+  font-size: 2.5vmax;
+}
+`;
 
 const Text4 = styled(Text1)`
 padding: 3vh 0;
+
+@media screen and (max-width: 1100px) {
+  font-size: 1.5vmax;
+  padding: 5% 15%;
+}
 `;
 
 const Button2 = styled(Text1)`
 color: #12679b;
 border-bottom: #12679b solid 2px;
 margin-bottom: 2.5vh;
+padding: 0;
 :hover {cursor: pointer;}
-`;
-
-const Note = styled.div`
-display: none;
-${props => props.clicked && css`
-display: block;
-`}
+@media screen and (max-width: 1100px) {
+  padding: 0;
+}
 `;
 
 class Section4 extends Component {
@@ -260,8 +367,10 @@ class Section4 extends Component {
       <Wrapper4>
         <Title4>HAVE AN OPEN LOT ?</Title4>
         <Text4>Help contribute to our cause and join in a meaningful partnership with Safe Parking LA.</Text4>
-        <Button2 onClick={this.handleClick} clicked={this.state.clicked}>Find out details</Button2>
-        <Note clicked={this.state.clicked}>Coming soon!</Note>
+        <Button2 onClick={this.handleClick} clicked={`${this.state.clicked}`}>Find out details</Button2>
+        {this.state.clicked ? 
+        <span clicked={`${this.state.clicked}`}>Coming soon!</span> : <span></span>
+        }
       </Wrapper4>
     )
   }
@@ -271,7 +380,7 @@ class HomePage extends Component {
     render() {
         return (
             <>
-            <Section1 image={image1}/>
+            <Section1 />
             <Section2 />
             <Section3 />
             <Section4 />
