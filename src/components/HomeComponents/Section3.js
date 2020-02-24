a00a3f37db68e489ac7e94c9c34040e3d2e819e9
Calvin Féau <calvin@calvins-mbp.lan>
2020-02-24 00:04:35 -0800

    finish laptop / desktop view of section 3 homepage

--- src/components/HomeComponents/Section3.js
+++ src/components/HomeComponents/Section3.js
@@ -6,145 +6,145 @@ import image4 from "../../assets/image4.png";
 
 
 const Wrapper3 = styled.div`
-width: 100vw;
-// height: 90vh;
-padding: 5vw;
-display: flex;
-justify-content: space-between;
+// width: 100vw;
+// // height: 90vh;
+// padding: 5vw;
+// display: flex;
+// justify-content: space-between;
 
-@media screen and (min-width: 2100px) {
-  justify-content: space-around;
-  height: 110vh;
-}
-@media screen and (max-width: 1100px) {
-  flex-direction: column;
-  padding: 0;
-  justify-content: start;
-  height: 100%;
-}
+// @media screen and (min-width: 2100px) {
+//   justify-content: space-around;
+//   height: 110vh;
+// }
+// @media screen and (max-width: 1100px) {
+//   flex-direction: column;
+//   padding: 0;
+//   justify-content: start;
+//   height: 100%;
+// }
 `;
 
 const Card = styled.div`
-width: 30%;
-// height: 100%;
-display: flex;
-flex-direction: column;
-align-items: center;
->img {
-  height: auto;
-  width: 100%;
-}
-${props => props.card === 1 && css`background-color: #FFEEDA;`}
-${props => props.card === 2 && css`background-color: #DBEDDD;`}
-${props => props.card === 3 && css`background-color: #FFF7E2;`}
+// width: 30%;
+// // height: 100%;
+// display: flex;
+// flex-direction: column;
+// align-items: center;
+// >img {
+//   height: auto;
+//   width: 100%;
+// }
+// ${props => props.card === 1 && css`background-color: #FFEEDA;`}
+// ${props => props.card === 2 && css`background-color: #DBEDDD;`}
+// ${props => props.card === 3 && css`background-color: #FFF7E2;`}
 
-@media screen and (min-width: 2100px) {
-  width: 25%;
-}
-@media screen and (max-width: 1100px) {
-  width: 100vw;
-  background-color: white;
-  height: auto;
-  margin-bottom: 10%;
-  >img {
-    height: auto;
-  }
-}
+// @media screen and (min-width: 2100px) {
+//   width: 25%;
+// }
+// @media screen and (max-width: 1100px) {
+//   width: 100vw;
+//   background-color: white;
+//   height: auto;
+//   margin-bottom: 10%;
+//   >img {
+//     height: auto;
+//   }
+// }
 `;
 
 const CardText = styled.div`
-height: 55%;
-padding: 10%;
-display: flex;
-flex-direction: column;
-align-items: center;
-line-height: 1.5;
-font-size: 1vmax;
-letter-spacing: 1.5px;
->span {
-  font-weight: 700;
-  font-size: 1.5vmax;
-  padding-bottom: 10%;
-}
+// height: 55%;
+// padding: 10%;
+// display: flex;
+// flex-direction: column;
+// align-items: center;
+// line-height: 1.5;
+// font-size: 1vmax;
+// letter-spacing: 1.5px;
+// >span {
+//   font-weight: 700;
+//   font-size: 1.5vmax;
+//   padding-bottom: 10%;
+// }
 
-@media screen and (min-width: 2100px) {
-  padding-bottom: 5%;
-}
-@media screen and (max-width: 1100px) {
-  padding: 5% 10%;
-  justify-content: start;
-  font-size: 1.8vmax;
-  text-align: center;
-  line-height: 2;
-  >span {
-    font-size: 2.5vmax;
-    padding-bottom: 5%;
-}
-@media screen and (max-width: 500px) {
-  padding: 5%;
-  font-size: 1.5vmax;
-}
-`;
+// @media screen and (min-width: 2100px) {
+//   padding-bottom: 5%;
+// }
+// @media screen and (max-width: 1100px) {
+//   padding: 5% 10%;
+//   justify-content: start;
+//   font-size: 1.8vmax;
+//   text-align: center;
+//   line-height: 2;
+//   >span {
+//     font-size: 2.5vmax;
+//     padding-bottom: 5%;
+// }
+// @media screen and (max-width: 500px) {
+//   padding: 5%;
+//   font-size: 1.5vmax;
+// }
+// `;
 
 const CardButton = styled.a`
-width: 75%;
-padding: 0.5vmax;
-margin-top: auto;
-background-color: transparent;
-color: #12679b;
-font-size: 0.8vmax;
-text-align: center;
-font-weight: 700;
-border: 1px solid #12679b;
-border-radius: 30px;
-:hover{
-  color: white; 
-  background-color: #12679b;
-  text-decoration: none;
-}
+// width: 75%;
+// padding: 0.5vmax;
+// margin-top: auto;
+// background-color: transparent;
+// color: #12679b;
+// font-size: 0.8vmax;
+// text-align: center;
+// font-weight: 700;
+// border: 1px solid #12679b;
+// border-radius: 30px;
+// :hover{
+//   color: white; 
+//   background-color: #12679b;
+//   text-decoration: none;
+// }
 
-@media screen and (max-width: 1100px) {
-  padding: 2%;
-  font-size: 1.8vmax;
-  width: 85%;
-  margin-top: 10%;
-  ${props => props.card === 1 && css`--current-color: #E59638;`}
-  ${props => props.card === 2 && css`--current-color: #297F32;`}
-  ${props => props.card === 3 && css`--current-color: #E2B731;`}
-  color: var(--current-color);
-  border-color: var(--current-color);
-  :hover{
-    color: white; 
-    background-color: var(--current-color);
-    text-decoration: none;
-  }
-}
-@media screen and (max-width: 500px) {
-  font-size: 1.5vmax;
-  width: 85%;
-  margin-top: 10%;
-  padding: 5%;
-  ${props => props.card === 1 && css`--current-color: #E59638;`}
-  ${props => props.card === 2 && css`--current-color: #297F32;`}
-  ${props => props.card === 3 && css`--current-color: #E2B731;`}
-  color: var(--current-color);
-  border-color: var(--current-color);
-  :hover{
-    color: white; 
-    background-color: var(--current-color);
-    text-decoration: none;
-  }
-}
+// @media screen and (max-width: 1100px) {
+//   padding: 2%;
+//   font-size: 1.8vmax;
+//   width: 85%;
+//   margin-top: 10%;
+//   ${props => props.card === 1 && css`--current-color: #E59638;`}
+//   ${props => props.card === 2 && css`--current-color: #297F32;`}
+//   ${props => props.card === 3 && css`--current-color: #E2B731;`}
+//   color: var(--current-color);
+//   border-color: var(--current-color);
+//   :hover{
+//     color: white; 
+//     background-color: var(--current-color);
+//     text-decoration: none;
+//   }
+// }
+// @media screen and (max-width: 500px) {
+//   font-size: 1.5vmax;
+//   width: 85%;
+//   margin-top: 10%;
+//   padding: 5%;
+//   ${props => props.card === 1 && css`--current-color: #E59638;`}
+//   ${props => props.card === 2 && css`--current-color: #297F32;`}
+//   ${props => props.card === 3 && css`--current-color: #E2B731;`}
+//   color: var(--current-color);
+//   border-color: var(--current-color);
+//   :hover{
+//     color: white; 
+//     background-color: var(--current-color);
+//     text-decoration: none;
+//   }
+// }
 `;
 
 const Section3 = () => {
     return ( 
-      <Wrapper3>
+      <Wrapper3 id="section3">
         <Card card={1}>
           <img src={image2} alt="couple checking hanfd with someone"/>
           <CardText>
-            <span>WHO WE SERVE</span>
-            In Los Angeles County, there are over 15,700 people living in their vehicles each night. These vehicle dwellers represent over 25% of the population of people experiencing homelessness&nbsp;in&nbsp;LA&nbsp;County.
+              <span>WHO WE SERVE</span>
+              In Los Angeles County, there are over 15,700 people living in their vehicles each night. These vehicle dwellers represent over 25% of the population of people experiencing homelessness&nbsp;in&nbsp;LA&nbsp;County.
           <CardButton card={1} href="https:www.safeparkingla.org/who-we-serve" target="_blank" rel="noopener noreferrer">READ MORE STORIES</CardButton>
           </CardText>
         </Card>
