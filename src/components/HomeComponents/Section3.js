4db5209c9985e1ce675dea86ac67abe30676b93f
Calvin Féau <calvin@calvins-mbp.lan>
2020-02-24 20:35:56 -0800

    continue section 3 responsivity

--- src/components/HomeComponents/Section3.js
+++ src/components/HomeComponents/Section3.js
@@ -144,7 +144,9 @@ const Section3 = () => {
           <img src={image2} alt="couple checking hanfd with someone"/>
           <CardText>
               <span>WHO WE SERVE</span>
-              In Los Angeles County, there are over 15,700 people living in their vehicles each night. These vehicle dwellers represent over 25% of the population of people experiencing homelessness&nbsp;in&nbsp;LA&nbsp;County.
+              <span>
+                In Los Angeles County, there are over 15,700 people living in their vehicles each night. These vehicle dwellers represent over 25% of the population of people experiencing homelessness&nbsp;in&nbsp;LA&nbsp;County.
+              </span>
           <CardButton card={1} href="https:www.safeparkingla.org/who-we-serve" target="_blank" rel="noopener noreferrer">READ MORE STORIES</CardButton>
           </CardText>
         </Card>
@@ -153,7 +155,9 @@ const Section3 = () => {
           <img src={image3} alt="man holding a dog"/>
           <CardText>
           <span>CAR BUT NO HOME?</span>
-          Safe Parking LA is an example of a relatively new effort on the West Coast to assist a specific group of individuals before they fall further to life on the street. Many of them may be working or have some source of income and are homeless for the&nbsp;first&nbsp;time. 
+          <span>
+            Safe Parking LA is an example of a relatively new effort on the West Coast to assist a specific group of individuals before they fall further to life on the street. Many of them may be working or have some source of income and are homeless for the&nbsp;first&nbsp;time. 
+          </span>
           <CardButton card={2} href="https:www.safeparkingla.org/news" target="_blank" rel="noopener noreferrer">READ MORE NEWS</CardButton>
           </CardText>
         </Card>
@@ -162,7 +166,9 @@ const Section3 = () => {
           <img src={image4} alt="veterans"/>
           <CardText>
           <span>FOR VETERANS</span>
-          SafeParkingLA provides a safe and legal overnight parking place for veterans and their families who are currently living in their vehicles. We are in a secure lot with a portable bathroom&nbsp;and&nbsp;wash&nbsp;station.
+          <span>
+            SafeParkingLA provides a safe and legal overnight parking place for veterans and their families who are currently living in their vehicles. We are in a secure lot with a portable bathroom&nbsp;and&nbsp;wash&nbsp;station.
+          </span>
           <CardButton card={3} href="https:www.safeparkingla.org/resources-for-veterans" target="_blank" rel="noopener noreferrer">MORE INFO</CardButton>
           </CardText>
         </Card>
