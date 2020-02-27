ab16d585617b789dbc4fd91a8299dededfaeb44e
Calvin Féau <calvin@calvins-mbp.lan>
2020-02-26 20:38:04 -0800

    finish section 3 homepage responsivity all devices

--- src/components/HomeComponents/Section2.js
+++ src/components/HomeComponents/Section2.js
@@ -1,16 +1,22 @@
 import React from 'react';
 
 const Section2 = () => {
-    return ( 
-      <div id="section2">
-        <div>The Process</div>
-        <div id="steps">
-          <div>STEP 1 <i className="material-icons">assignment</i> Fill out safe parking&nbsp;form</div>
-          <div>STEP 2 <i className="material-icons">headset_mic</i> We will contact you&nbsp;in&nbsp;24&nbsp;hrs</div>
-          <div>STEP 3 <i className="material-icons">local_parking</i> Receive a parking&nbsp;permit</div>
+  return ( 
+    <div id="section2">
+      <div>The Process</div>
+      <div id="steps">
+        <div>
+          STEP 1 <i className="material-icons">assignment</i> Fill out safe parking&nbsp;form
+        </div>
+        <div>
+          STEP 2 <i className="material-icons">headset_mic</i> We will contact you&nbsp;in&nbsp;24&nbsp;hrs
+        </div>
+        <div>
+          STEP 3 <i className="material-icons">local_parking</i> Receive a parking&nbsp;permit
         </div>
       </div>
-     );
+    </div>
+  );
 };
  
 export default Section2;
